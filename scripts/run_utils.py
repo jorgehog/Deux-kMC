7bc9d143ca12ac7f486735bb346b24beca7bb1b7
jorgehog <jorgen_da_mad@hotmail.com>
2015-03-03 20:34:00 +0100

    updated runscript

--- scripts/run_utils.py
+++ scripts/run_utils.py
@@ -21,7 +21,7 @@ def run_kmc(proc, combination, path, app, cfg):
     this_dir = os.getcwd()
 
     os.chdir(path)
-    success = os.system("./%s %d %s_%d.cfg >> /tmp/kmc_dump_%d.txt" % (app, proc, cfg.strip(".cfg"), proc, proc))
+    success = os.system("./%s %d %s_%d.cfg >> /tmp/kmc_dump_%d.txt" % (app, proc, cfg.split(".cfg")[0], proc, proc))
     os.chdir(this_dir)
 
     return success
