0ad64421091fc7029ac3f79f769fa2ec974cebe2
Jørgen Høgberget <jorgen_da_mad@hotmail.com>
2015-04-07 22:22:06 +0200

    moved a dcviz class to separate file

--- scripts/SOS_steady_state/dcviz.py
+++ scripts/SOS_steady_state/dcviz.py
@@ -1,5 +1,8 @@
 from DCViz_sup import DCVizPlotter
 
+import numpy as np
+from numpy import exp
+
 class SteadyState(DCVizPlotter):
 
     nametag = "steadystate\_(.*)\.npy"
@@ -65,3 +68,58 @@ class SteadyState(DCVizPlotter):
         self.subfigure.legend(loc="lower right")
         self.subfigure.set_xlim(0, self.subfigure.get_xlim()[1])
         self.subfigure.set_ylim(0, 1.1)
+
+
+class GrowthSpeed(DCVizPlotter):
+
+    nametag = "growthspeed\_(.*)\.npy"
+
+    numpyBin = True
+
+    isFamilyMember = True
+
+    hugifyFonts = True
+
+    def plot(self, data):
+
+
+        E0_values = data[self.get_family_index_from_name("growthspeed_E0.npy")].data
+        alpha_values = data[self.get_family_index_from_name("growthspeed_alpha.npy")].data
+        mu_shift_values = data[self.get_family_index_from_name("growthspeed_mu_shift.npy")].data
+        mu_values = data[self.get_family_index_from_name("growthspeed_mu.npy")].data
+        v_values = data[self.get_family_index_from_name("growthspeed_v.npy")].data
+
+        V = []
+
+        marks = ["s--", "^--", "v--"]
+
+        count = 0
+        for i, E0 in enumerate(E0_values):
+            V.append([])
+            MU = []
+            for j, alpha in enumerate(alpha_values):
+                for k, mu_shift in enumerate(mu_shift_values):
+                    if j == 2:
+
+                        print count
+                        V[i].append(v_values[count])
+                        MU.append(mu_values[count])
+
+                    count += 1
+
+            MU = np.array(MU)
+            V[i] = np.array(V[i])*exp(MU)
+
+            c_over_ceq = exp(mu_shift_values)
+
+            self.subfigure.plot(c_over_ceq-1, V[i], "--" + marks[i], label="E0/L=%g" % E0)
+
+        omega0 = np.load("/tmp/omega0.npy")
+        v0 = np.load("/tmp/v0.npy")
+
+        self.subfigure.plot(omega0, v0, "k*", label="E0/L=0")
+        om = np.linspace(c_over_ceq.min(), c_over_ceq.max()) - 1
+        # self.subfigure.plot(om, 0.01*om*(exp(om*1.01) - 1))
+        self.subfigure.set_xlabel(r"$\Omega$")
+        self.subfigure.set_ylabel(r"$\langle v\rangle$")
+        self.subfigure.legend(loc="lower right")
