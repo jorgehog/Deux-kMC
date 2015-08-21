0a0fd054fdecb444fd4ba69fceb44045277b3621
jorgehog <jorgen_da_mad@hotmail.com>
2015-08-21 16:41:43 +0200

    added base for selecting concentration boundary event

--- src/soskmc/concentrationboundaryreaction.cpp
+++ src/soskmc/concentrationboundaryreaction.cpp
@@ -1,14 +1,18 @@
 #include "concentrationboundaryreaction.h"
 #include "sossolver.h"
 #include "Events/diffusion/diffusion.h"
+#include "Events/confiningsurface/confiningsurface.h"
+
 
 concentrationBoundaryReaction::concentrationBoundaryReaction(const uint dim, const uint orientation, SOSSolver &solver) :
     Reaction(),
     m_dim(dim),
     m_orientation(orientation),
+    m_location(orientation == 0 ? 0 : (dim == 0 ? solver.length() : solver.width())),
+    m_span(dim == 0 ? solver.length() : solver.width()),
     m_solver(solver)
 {
-
+    //location = 0 if orientation = 0 for both dims. If location = 1 we are at the end of the respective dim.
 }
 
 concentrationBoundaryReaction::~concentrationBoundaryReaction()
@@ -16,21 +20,55 @@ concentrationBoundaryReaction::~concentrationBoundaryReaction()
 
 }
 
+double concentrationBoundaryReaction::freeBoundaryArea() const
+{
+    double surfaceHeight = solver().confiningSurfaceEvent().height();
+    int surfaceHeightInt = (int)surfaceHeight;
+
+    double area = span()*(surfaceHeight - surfaceHeightInt);
+
+    for (uint xi = 0; xi < span(); ++xi)
+    {
+        for (int z = base(xi); z < surfaceHeightInt; ++z)
+        {
+            if (!AXTRANS(solver().diffusionEvent().isBlockedPosition, xi, z))
+            {
+                area += 1;
+            }
+        }
+    }
+
+    return area;
+}
+
+double concentrationBoundaryReaction::dh(const uint n) const
+{
+    const double &surfaceHeight = solver().confiningSurfaceEvent().height();
+
+    return surfaceHeight - base(n);
+}
+
+const int &concentrationBoundaryReaction::base(const uint n) const
+{
+    return AXTRANS(solver().height, n);
+}
+
 bool concentrationBoundaryReaction::isAllowed() const
 {
-    return !solver().diffusionEvent().isBlockedPosition(x(), y(), z());
+    return freeBoundaryArea() != 0;
 }
 
 void concentrationBoundaryReaction::executeAndUpdate()
 {
     //choose z randomly across boundary. Subtract Nb and find correct z.
 
-    solver().diffusionEvent().insertDiffusingParticle(x(), y(), z());
+    uint x = 0;
+    uint y = 0;
+    int z = 0;
+    solver().diffusionEvent().insertDiffusingParticle(x, y, z);
 }
 
 double concentrationBoundaryReaction::rateExpression()
 {
-    //find dh and nb.
-
-    return (dh - Nb)/(1-exp(solver().gamma() - 2*solver().alpha()));
+    return freeBoundaryArea()/(1-exp(solver().gamma() - 2*solver().alpha()));
 }
--- src/soskmc/concentrationboundaryreaction.h
+++ src/soskmc/concentrationboundaryreaction.h
@@ -1,18 +1,66 @@
 #pragma once
 
-#include "sosreaction.h"
+#include "sossolver.h"
+#include "../kmcsolver/reaction.h"
 
-class concentrationBoundaryReaction : public SOSReaction
+#define AXTRANS(callable, n, ...) dim() == 0 ? callable(n, location(), ##__VA_ARGS__) : callable(location(), n, ##__VA_ARGS__)
+
+class concentrationBoundaryReaction : public kMC::Reaction
 {
 public:
     concentrationBoundaryReaction(const uint dim, const uint orientation, SOSSolver &solver);
     ~concentrationBoundaryReaction();
 
-    double freeBoundaryArea();
+    double freeBoundaryArea() const;
+
+    double dh(const uint n) const;
+
+    const int &base(const uint n) const;
+
+    const uint &dim() const
+    {
+        return m_dim;
+    }
+
+    const uint &orientation() const
+    {
+        return m_orientation;
+    }
+
+    const uint &location() const
+    {
+        return m_location;
+    }
+
+    const SOSSolver &solver() const
+    {
+        return m_solver;
+    }
+
+    const uint &span() const
+    {
+        return m_span;
+    }
+
+    template<class Callable, class InputType>
+    auto axisTransform(Callable &&callable, InputType && input) const -> decltype(callable(input, input))
+    {
+        if (dim() == 0)
+        {
+            return callable(std::forward<InputType>(input), location());
+        }
+
+        else
+        {
+            return callable(location(), std::forward<InputType>(input));
+        }
+    }
 
 private:
     const uint m_dim;
     const uint m_orientation;
+    const uint m_location;
+    const uint m_span;
 
     SOSSolver &m_solver;
 
