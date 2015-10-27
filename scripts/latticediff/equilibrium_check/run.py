b3cd970f1108c1ac69332f383e6d074dd5cc490c
jorgehog <jorgen_da_mad@hotmail.com>
2015-10-27 18:18:03 +0100

    reverted to other volume calculation.

--- scripts/latticediff/equilibrium_check/run.py
+++ scripts/latticediff/equilibrium_check/run.py
@@ -13,15 +13,15 @@ def main():
     controller, path, app, cfg, n_procs = parse_input(sys.argv)
 
     supersaturation_values = ParameterSet(cfg, "supersaturation\s*\=\s*(.*)\;")
-    super_saturation = np.linspace(-2./3, 2./3, 3)
+    super_saturation = np.linspace(-2./3, 2./3, 7)
     supersaturation_values.initialize_set(super_saturation)
 
-    n = 8
+    n = 16
 
     controller.register_parameter_set(supersaturation_values)
     controller.set_repeats(n)
 
-    success = controller.run(run_kmc, path, app, cfg, ask=not controller.use_mpi, n_procs=n_procs)
+    success = controller.run(run_kmc, path, app, cfg, ask=not controller.use_mpi, n_procs=n_procs, shuffle=True)
 
     if success != 0:
         print "Simulation failed."
@@ -31,3 +31,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
