70662e5ad52d3a2940a0439e4d7872ff6493f423
Jørgen Høgberget <jorgen_da_mad@hotmail.com>
2016-06-02 16:21:43 +0200

    cluster script updates

--- scripts/extraneighbor_clusters/analyze.py
+++ scripts/extraneighbor_clusters/analyze.py
@@ -1,14 +1,62 @@
 import sys
 import os
 from os.path import join
-from numpy import where, empty, save
+import numpy as np
+from numpy import where, empty, save, zeros, unique
 from skimage import measure
 from threading import Thread
+from matplotlib import pylab as plab
 
 sys.path.append(join(os.getcwd(), ".."))
 
 from parse_h5_output import ParseKMCHDF5
 
+min_area = 4
+
+def find_perimeters(pim, labels):
+    L, W = pim.shape
+    expanded = zeros(shape=[3*d for d in pim.shape], dtype=int)
+    for x in range(3):
+        for y in range(3):
+            expanded[x*L:(x+1)*L, y*W:(y+1)*W] = pim
+
+    n_labels = len(labels)
+
+    idx_map = {}
+
+    n = 0
+    for l in labels:
+        idx_map[l] = n
+        n += 1
+
+    winners = [None for _ in range(n_labels)]
+    winner_areas = [0 for _ in range(n_labels)]
+
+    l = measure.label(expanded)
+    l[np.where(expanded == 0)] = -1
+    l += 1
+    #figure()
+    #pcolor(l)
+
+    def find_fast(im, v, x0, y0, x1, y1):
+        for x in range(x0, x1):
+            for y in range(y0, y1):
+                if im[x, y] == v:
+                    return x, y
+
+    expanded_props = measure.regionprops(l)
+    for prop in expanded_props:
+
+        x, y = find_fast(l, prop.label, *prop.bbox)
+        orig_label = expanded[x, y]
+
+        idx = idx_map[orig_label]
+
+        if prop.area > winner_areas[idx]:
+            winner_areas[idx] = prop.area
+            winners[idx] = [orig_label, (prop.perimeter, prop.eccentricity)]
+
+    return dict(winners)
 
 def changefill(im, source, dest):
     if dest == -1 or source == -1:
@@ -42,6 +90,7 @@ def analyze(data, i,
             avg_cluster_sizes,
             n_clusters_list,
             avg_circs,
+            eccentricities,
             n_broken,
             n_gained):
 
@@ -52,32 +101,44 @@ def analyze(data, i,
     im_label = measure.label(_s)
     im_label[where(_s == 0)] = -1
 
-    im_label_per = periodify(im_label)
+    im_label_per = periodify(im_label) + 1
 
     props = measure.regionprops(im_label_per)
 
     all_areas = []
     all_circs = []
+    all_eccs = []
 
-    for m in props:
-        if m.label != -1:
-            all_areas.append(m.area)
-            all_circs.append(m.perimeter)
+    if props:
+        perimeter_data = find_perimeters(im_label_per, [prop.label for prop in props])
+
+        for m in props:
+            if m.label != 0 and m.area > min_area:
+                all_areas.append(m.area)
+                all_circs.append(perimeter_data[m.label][0])
+                all_eccs.append(perimeter_data[m.label][1])
 
     n_clusters = len(all_areas)
 
     if n_clusters == 0:
         avg_cluster_size = 0
         avg_circ = 0
+        avg_eccs = 0
     else:
         avg_cluster_size = sum(all_areas)/len(all_areas)
         avg_circ = sum(all_circs)/len(all_circs)
+        avg_eccs = sum(all_eccs)/len(all_eccs)
 
     avg_cluster_sizes[i] = avg_cluster_size
     n_clusters_list[i] = n_clusters
     avg_circs[i] = avg_circ
+    eccentricities[i] = avg_eccs
 
     if i == data.shape[0] - 1:
+
+        plab.pcolor(_s)
+        plab.show()
+
         return
 
     _s_next = data[i+1, :, :]
@@ -86,6 +147,8 @@ def analyze(data, i,
 
     n_broken[i] = len(where(delta == -1)[0])
     n_gained[i] = len(where(delta == 1)[0])
+
+
 
 
 class AnalyzeThread(Thread):
@@ -121,7 +184,8 @@ def main():
 
         print L, W, run_id
 
-        coverage_matrix = data["eq_coverage_matrix"][()][::every]
+        #coverage_matrix = data["eq_coverage_matrix"][()][::every]
+        coverage_matrix = np.load("/tmp/eq_coverage_new.npy")[::every]
 
         n = coverage_matrix.shape[0]
 
@@ -129,6 +193,7 @@ def main():
         avg_cluster_sizes = empty(n)
         n_clusters_list = empty(n)
         avg_circs = empty(n)
+        eccentricities = empty(n)
         n_broken = empty(n - 1)
         n_gained = empty(n - 1)
 
@@ -136,35 +201,41 @@ def main():
                     avg_cluster_sizes,
                     n_clusters_list,
                     avg_circs,
+                    eccentricities,
                     n_broken,
                     n_gained]
 
-        all_threads = []
-        all_i = range(n)
-        all_given_i = []
-        for thread in range(nThreads):
+        if nThreads != 1:
+            all_threads = []
+            all_i = range(n)
+            all_given_i = []
+            for thread in range(nThreads):
 
-            if thread != nThreads - 1:
-                thread_i = all_i[thread::nThreads]
-                all_given_i += thread_i
-            else:
-                thread_i = []
-                for i in all_i:
-                    if i not in all_given_i:
-                        thread_i.append(i)
+                if thread != nThreads - 1:
+                    thread_i = all_i[thread::nThreads]
+                    all_given_i += thread_i
+                else:
+                    thread_i = []
+                    for i in all_i:
+                        if i not in all_given_i:
+                            thread_i.append(i)
 
-            all_threads.append(AnalyzeThread(coverage_matrix, thread_i, *measures))
+                all_threads.append(AnalyzeThread(coverage_matrix, thread_i, *measures))
 
-        for thread in all_threads:
-            thread.start()
+            for thread in all_threads:
+                thread.start()
 
-        for thread in all_threads:
-            thread.join()
+            for thread in all_threads:
+                thread.join()
+        else:
+            for i in range(n):
+                analyze(coverage_matrix, i, *measures)
 
         save("/tmp/extran_cluster_covs.npy", covs)
         save("/tmp/extran_cluster_size.npy", avg_cluster_sizes)
         save("/tmp/extran_cluster_n.npy", n_clusters_list)
         save("/tmp/extran_cluster_circs.npy", avg_circs)
+        save("/tmp/extran_cluster_eccs.npy", eccentricities)
         save("/tmp/extran_cluster_nbroken.npy", n_broken)
         save("/tmp/extran_cluster_ngained.npy", n_gained)
 
