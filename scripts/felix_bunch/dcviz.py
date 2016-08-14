84d105fcbbac6ec9d93e8da6578586a95c37779f
Jørgen Høgberget <jorgen_da_mad@hotmail.com>
2016-08-14 20:03:14 +0200

    script updates

--- scripts/felix_bunch/dcviz.py
+++ scripts/felix_bunch/dcviz.py
@@ -29,30 +29,36 @@ class flx_bunch(DCVizPlotter):
     fig_size = [6, 8]
 
     specific_fig_size = {"figure": [6, 6],
-                         "vfigure": [6, 4]}
+                         "vfigure": [6, 4],
+                         "twostepfig": [6, 4]}
 
     figMap = {"figure": ["subfigure", "subfigure_combo"],
               "vfigure": "vfig",
               "speedfig": "sfig",
               "allfig": "afig",
               "allfig2": "afig2",
-              "allfig3": "afig3"}
+              "twostepfig": "afig3"}
 
     def adjust(self):
-        fig = "figure"
         l = 0.16
-        self.adjust_maps[fig]['top'] = 0.97
-        self.adjust_maps[fig]['bottom'] = 0.13
-        self.adjust_maps[fig]['left'] = l
-        self.adjust_maps[fig]['right'] = 1-l
-        self.adjust_maps[fig]['hspace'] = 0.05
+        for fig in ["figure", "twostepfig", "vfigure"]:
+            self.adjust_maps[fig]['left'] = l
+            self.adjust_maps[fig]['right'] = 1-l
+            self.adjust_maps[fig]['hspace'] = 0.05
 
-    plotOnly = "allfig3"
+        self.adjust_maps["figure"]['top'] = 0.97
+        self.adjust_maps["figure"]['bottom'] = 0.13
+
+        self.adjust_maps["twostepfig"]['top'] = 0.95
+        self.adjust_maps["twostepfig"]['bottom'] = 0.2
+
+
+    #plotOnly = "twostepfig"
 
     def find_half(self, t):
 
         i = 0
-        while t[i] < 0.5:
+        while t[i] < 0.9:
             i+=1
 
         return i
@@ -105,13 +111,12 @@ class flx_bunch(DCVizPlotter):
 
             T += t
 
-        assume_twos = True
-        lim = 0.03
-        back = 5
+        assume_twos = 3 in skip
+        lim = 0.025
+        back = 0
         get_s = lambda xvals: np.where(xvals < lim)[0][-1] - back
         if assume_twos:
-            combinator = ICZ("Time", "h1")
-            combinator2 = ICZ("Time", "h2")
+            combinator = ICZ("Time", "h1", "h2")
 
             for i in range(nr):
 
@@ -129,14 +134,47 @@ class flx_bunch(DCVizPlotter):
                 self.afig.plot(t_trans, x1)
                 self.afig2.plot(t_trans2, x2)
 
-                combinator.feed(t_trans/t[s2], x1)
-                combinator2.feed(t_trans2/t[s2], x2)
+                combinator.feed(t_trans/(t[s2]-t[s1]), x1, x2)
             t1, h1 = combinator.intercombine("Time", "h1")
-            t2, h2 = combinator2.intercombine("Time", "h2")
+            _, h2 = combinator.intercombine("Time", "h2")
 
-            self.afig3.plot(t1, h1)
-            self.afig3.plot(t2+1, h2)
-            self.afig3.plot(t2, h2*h1[self.find_half(t1)]/h2[self.find_half(t2)])
+            xmax = 12
+            self.afig3.plot(t1, h1, "r-", label=r"$y_s^{(1)}(t')$")
+            scale = h1[self.find_half(t1)]/h2[self.find_half(t1-1)]
+            self.afig3.plot(t1-1, h2*scale, "k--", label=r"$%.2fy_s^{(2)}(t'-1)$" % scale)
+            self.afig3.plot(t1, h2, "b-", linewidth=2, label=r"$y_s^{(2)}(t')$")
+
+            l = self.afig3.legend(loc="center",
+                   numpoints=1,
+                   ncol=2,
+                   handlelength=1.25,
+                   borderpad=0.2,
+                   labelspacing=0.2,
+                   columnspacing=-2.5,
+                   handletextpad=0.25,
+                   borderaxespad=0.0,
+                   frameon=False,
+                   fontsize=20,
+                   bbox_to_anchor=(0.625, 0.2))
+
+            l.get_frame().set_fill(not (self.toFile and self.transparent))
+
+            self.afig3.set_xlim(-0.5, xmax)
+            self.afig3.set_xlabel(r"$t'=(t-t_1)/(t_2-t_1)$")
+            self.afig3.set_xticks(range(xmax+1))
+            self.afig3.set_ylabel(r"$y_s/L_y$")
+            self.afig3.set_ylim(0, 0.8)
+
+            # def haxxorformatter(v, _):
+            #     if v == 0:
+            #         return r"$0$"
+            #     else:
+            #         if v % 2 == 0:
+            #             return ""
+            #         else:
+            #             return r"$%g$" % v
+
+            # self.afig3.xaxis.set_major_formatter(FuncFormatter(haxxorformatter))
 
         seq_levels = []
         for hlevel in range(1, nl):
@@ -169,10 +207,10 @@ class flx_bunch(DCVizPlotter):
         self.subfigure_combo.set_ylim(0, all_x[:, 1:, :].max()+0.01)
 
 
-        self.subfigure_combo.set_ylabel(r"$y/L_y$")
+        self.subfigure_combo.set_ylabel(r"$y_s/L_y$")
         self.subfigure_combo.set_xlabel(r"$t/t_\mathrm{end}$")
 
-        self.subfigure.set_ylabel(r"$y/L_y$")
+        self.subfigure.set_ylabel(r"$y_s/L_y$")
 
         self.subfigure.xaxis.set_major_formatter(GFORMATTER)
         self.subfigure.yaxis.set_major_formatter(GFORMATTER)
